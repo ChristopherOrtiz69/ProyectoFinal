19b549b39845a9f43eaea9573eeee5f4b55d3bea
Christopher Ivan <cdmx2224@amerike.edu.mx>
2022-11-15 10:28:38 -0600

    first commit

---
+++ schris.sql
@@ -0,0 +1,15 @@
+
+
+
+USE F1;
+
+CREATE TABLE pilotos(
+    pilotos_id INTEGER UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(10) NOT NULL,
+    apeliido VARCHAR(10) NOT NULL,
+    nacionalidad VARCHAR(11) NOT NULL,
+    
+
+
+)
+
